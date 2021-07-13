f93a61049397c8243b6f875ddb68ad3a3f33c94c
Mubashir <mohammedmubashir204@gmail.com>
2021-07-13 17:01:06 +0530

    Complete Nav bar and this is the first commit

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" />
+            <NavMenu>
+                <a href>
+                    <img src="/images/home-icon.svg" alt="" />
+                    <span>HOME</span>
+                </a>
+                <a href>
+                    <img src="/images/search-icon.svg" alt="" />
+                    <span>SEARCH</span>
+                </a>
+                <a href>
+                    <img src="/images/watchlist-icon.svg" alt="" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href>
+                    <img src="/images/original-icon.svg" alt="" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href>
+                    <img src="/images/movie-icon.svg" alt="" />
+                    <span>MOVIES</span>
+                </a>
+                <a href>
+                    <img src="/images/series-icon.svg" alt="" />
+                    <span>SERIES</span>
+                </a>
+
+            </NavMenu>
+            <UserImg src="https://pbs.twimg.com/profile_images/1344728941383176192/Ob_PyZ7Z_400x400.jpg" />
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.nav`
+    height:70px ;
+    background-color: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+
+`
+
+const Logo = styled.img`
+    width: 80px;
+
+`
+
+const NavMenu = styled.div`
+display: flex;
+flex: 1;
+margin-left: 20px;
+align-items: center;
+
+a{
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img{
+        height: 20px;
+    }
+    span{
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        position: relative;
+
+        &:after{
+            content: "";
+            height: 2px;
+            background-color: white;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom:-6px ; 
+            transform-origin : left center;
+            transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+            opacity:0;
+            transform: scaleX(0);
+        }
+    }
+    &:hover{
+        span:after{
+            transform: scaleX(1);
+            opacity: 1;
+        }
+    }
+}
+`
+const UserImg = styled.img`
+width: 48px;
+height: 48px;
+cursor: pointer;
+border-radius: 50%;
+`
